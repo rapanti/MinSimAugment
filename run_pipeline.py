a9a6df818db205bc12345a7d8028efa352ca9282
fabioferreira <fabiof@cs.stanford.edu>
2023-07-15 13:21:32 +0200

    fix for 'Address already in use error'

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,11 +1,15 @@
 from omegaconf import OmegaConf
+from utils import find_free_port
 
 import eval_linear
 import pretrain
 
 
 if __name__ == "__main__":
+    # Define master port (for preventing 'Address already in use error' when submitting more than 1 jobs on 1 node)
+    master_port = find_free_port()
     cfg = OmegaConf.load('pretrain.yaml')
+    cfg.dist_url = "tcp://localhost:" + str(master_port)
     print('STARTING PRETRAINING')
     pretrain.main(cfg)
 
@@ -14,5 +18,6 @@ if __name__ == "__main__":
     eval_cfg.gpu = cfg.gpu
     eval_cfg.rank = cfg.rank
     eval_cfg.world_size = cfg.world_size
+    eval_cfg.dist_url = cfg.dist_url
     print('STARTING EVALUATION')
     eval_linear.main(eval_cfg)
--- utils.py
+++ utils.py
@@ -350,3 +350,13 @@ def cancel_gradients_last_layer(epoch, model, freeze_last_layer):
     for n, p in model.named_parameters():
         if "last_layer" in n:
             p.grad = None
+
+
+def find_free_port():
+    import socket
+    from contextlib import closing
+
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
