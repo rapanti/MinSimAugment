f0812f1a447a88da4accd8f48e5761b5bbbd0f3f
fabioferreira <fabiof@cs.stanford.edu>
2023-08-24 15:11:33 +0200

    start kNN after linear eval

--- run_pipeline.py
+++ run_pipeline.py
@@ -15,6 +15,15 @@ if __name__ == "__main__":
     print('STARTING PRETRAINING')
     pretrain.main(cfg)
 
+    print('STARTING LINEAR EVAL EVALUATION')
+    eval_linear_cfg = OmegaConf.load("eval_linear.yaml")
+    # copy dist parameters
+    eval_linear_cfg.gpu = cfg.gpu
+    eval_linear_cfg.rank = cfg.rank
+    eval_linear_cfg.world_size = cfg.world_size
+    eval_linear_cfg.dist_url = cfg.dist_url
+    eval_linear.main(eval_linear_cfg)
+
     print('STARTING kNN EVALUATION')
     eval_knn_cfg = OmegaConf.load("eval_knn.yaml")
     # copy dist parameters
@@ -24,12 +33,3 @@ if __name__ == "__main__":
     eval_knn_cfg.dist_url = cfg.dist_url
 
     eval_knn.main(eval_knn_cfg)
-
-    print('STARTING LINEAR EVAL EVALUATION')
-    eval_linear_cfg = OmegaConf.load("eval_linear.yaml")
-    # copy dist parameters
-    eval_linear_cfg.gpu = cfg.gpu
-    eval_linear_cfg.rank = cfg.rank
-    eval_linear_cfg.world_size = cfg.world_size
-    eval_linear_cfg.dist_url = cfg.dist_url
-    eval_linear.main(eval_linear_cfg)
