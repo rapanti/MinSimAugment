7dc979d52c7674603040059d6885b72c8e174b8f
fabioferreira <fabiof@cs.stanford.edu>
2023-09-02 19:24:15 +0200

    added support for linearly evaluating StanfordCars and Flowers102

--- run_pipeline.py
+++ run_pipeline.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     print('STARTING PRETRAINING')
     pretrain.main(cfg)
 
-    print('STARTING LINEAR EVAL EVALUATION')
+    print('STARTING LINEAR EVAL EVALUATION ImageNet')
     eval_linear_cfg = OmegaConf.load("eval_linear.yaml")
     # copy dist parameters
     eval_linear_cfg.gpu = cfg.gpu
@@ -24,26 +24,48 @@ if __name__ == "__main__":
     eval_linear_cfg.dist_url = cfg.dist_url
     eval_linear.main(eval_linear_cfg)
 
-    print('STARTING LINEAR EVAL EVALUATION iNaturalist')
-    eval_linear_cfg = OmegaConf.load("eval_linear.yaml")
-    # copy dist parameters
-    eval_linear_cfg.dataset = "inat18"
+    print('STARTING LINEAR EVAL EVALUATION CIFAR10')
+    eval_linear_cfg.dataset = "CIFAR10"
+    eval_linear_cfg.batch_size = eval_linear_cfg.batch_size
+    eval_linear_cfg.weight_decay = 0.05
+    eval_linear_cfg.lr = 1e-6
+    eval_linear_cfg.epochs = 1000
+    eval_linear_cfg.optimizer = "AdamW"
+    eval_linear_cfg.data_path = "../datasets"
+    eval_linear.main(eval_linear_cfg)
+
+    print('STARTING LINEAR EVAL EVALUATION CIFAR100')
+    eval_linear_cfg.dataset = "CIFAR100"
+    eval_linear_cfg.batch_size = eval_linear_cfg.batch_size
+    eval_linear_cfg.weight_decay = 0.05
+    eval_linear_cfg.lr = 1e-6
+    eval_linear_cfg.epochs = 1000
+    eval_linear_cfg.optimizer = "AdamW"
     eval_linear_cfg.data_path = "../datasets"
     eval_linear.main(eval_linear_cfg)
 
     print('STARTING LINEAR EVAL EVALUATION Flowers102')
-    eval_linear_cfg = OmegaConf.load("eval_linear.yaml")
-    # copy dist parameters
     eval_linear_cfg.dataset = "Flowers102"
-
     eval_linear_cfg.batch_size = eval_linear_cfg.batch_size//2
+    eval_linear_cfg.weight_decay = 0.05
+    eval_linear_cfg.lr = 1e-6
+    eval_linear_cfg.epochs = 1000
+    eval_linear_cfg.optimizer = "AdamW"
     eval_linear_cfg.data_path = "../datasets"
     eval_linear.main(eval_linear_cfg)
 
-    print('STARTING LINEAR EVAL EVALUATION StanfordCars')
-    eval_linear_cfg = OmegaConf.load("eval_linear.yaml")
-    # copy dist parameters
-    eval_linear_cfg.dataset = "StanfordCars"
-    eval_linear_cfg.batch_size = eval_linear_cfg.batch_size // 2
+    print('STARTING LINEAR EVAL EVALUATION iNaturalist')
+    eval_linear_cfg.dataset = "inat18"
+    eval_linear_cfg.weight_decay = 0.05
+    eval_linear_cfg.lr = 5e-5
+    eval_linear_cfg.epochs = 100
+    eval_linear_cfg.optimizer = "AdamW"
     eval_linear_cfg.data_path = "../datasets"
     eval_linear.main(eval_linear_cfg)
+
+    # TODO: source currently offline
+    # print('STARTING LINEAR EVAL EVALUATION StanfordCars')
+    # eval_linear_cfg.dataset = "StanfordCars"
+    # eval_linear_cfg.batch_size = eval_linear_cfg.batch_size
+    # eval_linear_cfg.data_path = "../datasets"
+    # eval_linear.main(eval_linear_cfg)
