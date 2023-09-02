e00f1018a11f477237d5aa43a03a08efeb7252ad
fabioferreira <fabiof@cs.stanford.edu>
2023-09-02 19:40:50 +0200

    added support for linearly evaluating StanfordCars and Flowers102

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     utils.distributed.init_distributed_mode(cfg)
     utils.fix_random_seeds(cfg.seed)
 
-    print('STARTING PRETRAINING')
+    print('*************STARTING PRETRAINING*************')
     pretrain.main(cfg)
 
-    print('STARTING LINEAR EVAL EVALUATION ImageNet')
+    print('*************STARTING LINEAR EVAL EVALUATION: ImageNet*************')
     eval_linear_cfg = OmegaConf.load("eval_linear.yaml")
     # copy dist parameters
     eval_linear_cfg.gpu = cfg.gpu
@@ -24,29 +24,29 @@ if __name__ == "__main__":
     eval_linear_cfg.dist_url = cfg.dist_url
     eval_linear.main(eval_linear_cfg)
 
-    print('STARTING LINEAR EVAL EVALUATION CIFAR10')
+    print('*************STARTING LINEAR EVAL EVALUATION: CIFAR10*************')
     eval_linear_cfg.dataset = "CIFAR10"
     eval_linear_cfg.batch_size = eval_linear_cfg.batch_size
     # eval_linear_cfg.weight_decay = 0.05
     # eval_linear_cfg.lr = 1e-6
     # eval_linear_cfg.epochs = 300
-    eval_linear_cfg.batch_size = 2048
+    eval_linear_cfg.batch_size = 1024
     # eval_linear_cfg.optimizer = "adamw"
     eval_linear_cfg.data_path = "../datasets"
     eval_linear.main(eval_linear_cfg)
 
-    print('STARTING LINEAR EVAL EVALUATION CIFAR100')
+    print('*************STARTING LINEAR EVAL EVALUATION: CIFAR100*************')
     eval_linear_cfg.dataset = "CIFAR100"
     eval_linear_cfg.batch_size = eval_linear_cfg.batch_size
     # eval_linear_cfg.weight_decay = 0.05
     # eval_linear_cfg.lr = 1e-6
     # eval_linear_cfg.epochs = 1000
-    eval_linear_cfg.batch_size = 2048
+    eval_linear_cfg.batch_size = 1024
     # eval_linear_cfg.optimizer = "adamw"
     eval_linear_cfg.data_path = "../datasets"
     eval_linear.main(eval_linear_cfg)
 
-    print('STARTING LINEAR EVAL EVALUATION Flowers102')
+    print('*************STARTING LINEAR EVAL EVALUATION: Flowers102*************')
     eval_linear_cfg.dataset = "Flowers102"
     # eval_linear_cfg.weight_decay = 0.05
     # eval_linear_cfg.lr = 1e-6
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     eval_linear_cfg.data_path = "../datasets"
     eval_linear.main(eval_linear_cfg)
 
-    print('STARTING LINEAR EVAL EVALUATION iNaturalist')
+    print('*************STARTING LINEAR EVAL EVALUATION: iNaturalist*************')
     eval_linear_cfg.dataset = "inat18"
     # eval_linear_cfg.weight_decay = 0.05
     # eval_linear_cfg.lr = 5e-5
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     eval_linear.main(eval_linear_cfg)
 
     # TODO: source currently offline
-    # print('STARTING LINEAR EVAL EVALUATION StanfordCars')
+    # print('*************STARTING LINEAR EVAL EVALUATION: StanfordCars*************')
     # eval_linear_cfg.dataset = "StanfordCars"
     # eval_linear_cfg.batch_size = eval_linear_cfg.batch_size
     # eval_linear_cfg.data_path = "../datasets"
