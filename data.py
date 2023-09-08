572fa1c60ca3ecdd7481a8a301661eaaf445b7e1
fabioferreira <fabiof@cs.stanford.edu>
2023-09-08 17:32:34 +0200

    added Places365

--- data.py
+++ data.py
@@ -122,7 +122,7 @@ def make_dataset(
         return INaturalist(root, download=False, version=version, transform=transform), 10000
     elif dataset == "Places365":
         split = "train-standard" if train else "val"
-        return Places365(root, download=True, split=split, transform=transform), 365
+        return Places365(root, download=False, split=split, transform=transform), 365
     elif dataset == 'ImageNet':
         root = os.path.join(root, 'train' if train else 'val')
         dataset = ImageFolder(root, transform=transform)
