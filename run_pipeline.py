1242703da62dbffb27c7035ba4de2f5a3e1cde65
fabioferreira <fabiof@cs.stanford.edu>
2023-09-09 02:12:46 +0200

    added new hypers for inat21

--- run_pipeline.py
+++ run_pipeline.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     eval_linear_cfg.lr = 5e-5
     eval_linear_cfg.weight_decay = 0.05
     eval_linear_cfg.optimizer = "adamw"
-    eval_linear_cfg.epochs = 300
+    eval_linear_cfg.epochs = 100
     eval_linear_cfg.data_path = "/work/dlclarge1/ferreira-simsiam/minsim_experiments/datasets/places365"
     eval_linear.main(eval_linear_cfg)
 
